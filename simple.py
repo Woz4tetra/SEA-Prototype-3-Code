062d22223e862d7983d1f2cd50b6477c50ffe114
Woz4tetra <woz4tetra@gmail.com>
2018-10-29 22:45:06 -0400

    motor controller works

--- simple.py
+++ simple.py
@@ -1,38 +1,21 @@
-# from smc import SMC
-# import time
-#
-# mc = SMC('/dev/cu.usbmodem142401', 115200)
-# # open serial port and exit safe mode
-# mc.init()
-#
-# # drive using 12b mode
-# mc.speed(1000)
-# time.sleep(3)
-# mc.speed(-1000)
-# time.sleep(3)
-#
-# # drive using 7b mode
-# mc.speed7b(100)
-# time.sleep(3)
-# mc.speed7b(-100)
-# time.sleep(3)
-#
-# # and stop motor
-# mc.stop()
+from smc import SMC
+import time
 
-# import serial
-# import time
-import os
+mc = SMC('/dev/serial/by-id/usb-Pololu_Corporation_Pololu_Simple_High-Power_Motor_Controller_18v15_33FF-6806-4D4B-3731-5147-1543-if00', 115200)
+# open serial port and exit safe mode
+mc.init()
 
-dev_folder = os.listdir("/dev")
-for entry in dev_folder:
-    if entry.find("usb") > -1:
-        print(entry)
-# 
-# s = serial.Serial("/dev/tty.usbmodem142401", 115200, timeout=1.0)
-# time.sleep(1.0)
-#
-# s.write((0x83,))
-# time.sleep(0.1)
-# s.write((0xe0,))
-# s.close()
+# drive using 12b mode
+mc.speed(1000)
+time.sleep(3)
+mc.speed(-1000)
+time.sleep(3)
+
+# drive using 7b mode
+mc.speed7b(100)
+time.sleep(3)
+mc.speed7b(-100)
+time.sleep(3)
+
+# and stop motor
+mc.stop()
